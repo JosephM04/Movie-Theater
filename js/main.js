cdd8d89a1e5851785b326e61c8b676fe8877ed1c
Joseph Méndez <j.mendez0434@gmail.com>
2023-05-30 21:46:47 -0500

    Header section completed, slider completed, started the listings seccion\ HTML:\ -Div with class 'menu-div' that will be shown when the button 'menu' is clicked. \ -Div class='carousel' with all needed to work. \ -div listings-tittle-container and listings-contianer added' \ CSS:\ -All the configurations for  menu-div, carousel and listings-tittle-container. \ JS:\ -configs for menu-div and carousel

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-//-----------------------------Header-----------------------------------------
+//--------------------------------------------------------Header---------------------------------------------------------------------
 
-//-----------------------------select city, movie----------------------------
+//--------------------select city, movie-----------
 const citySelect = document.getElementById("select-city");
 const cinemaSelect = document.getElementById("select-cinema");
 
@@ -25,14 +25,111 @@ citySelect.addEventListener("change", () => {
   }
 });
 
-//----------------------------"menu" button----------------------------------
+//--------------------------------"menu" button---------------------------------
 
 const menuButton = document.querySelector(".menu-button ");
 const menuArrow = document.querySelector(".bxs-chevron-down");
+const clickMenu = document.querySelector(".menu-div");
 
-menuButton.addEventListener("click", () =>{
-    menuArrow.classList.toggle("bxs-chevron-up");
-}); 
+// Event when click menu button show the window
+menuButton.addEventListener("click", () => {
+  menuArrow.classList.toggle("bxs-chevron-up");
+  clickMenu.classList.toggle("show");
+});
+
+// Event to control when the windows have to be closed
+window.addEventListener('click', e => {
+  if (clickMenu.classList.contains('show') && e.target != clickMenu && e.target != menuButton) {
+    clickMenu.classList.toggle("show");
+  }
+});
+
+// ------------------------------------------------------ Body ------------------------------------------------------
+
+// ---------------------------Main Slider --------------------------------
+
+var carouselContainer = document.querySelector('.carousel-container');
+var carouselImages = document.querySelectorAll('.carousel-container img');
+var prevButton = document.querySelector('.carousel-control.prev');
+var nextButton = document.querySelector('.carousel-control.next');
+var dots = document.querySelectorAll('.carousel-dots .dot');
+var currentIndex = 0;
+var timer;
+
+// Función para cambiar a la siguiente imagen
+function nextSlide() {
+  currentIndex++;
+  if (currentIndex >= carouselImages.length) {
+    currentIndex = 0;
+  }
+  updateCarousel();
+}
+
+// Función para cambiar a la imagen anterior
+function prevSlide() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = carouselImages.length - 1;
+  }
+  updateCarousel();
+}
+
+// Función para actualizar el carrusel con la imagen actual
+function updateCarousel() {
+  var translateValue = -currentIndex * carouselContainer.offsetWidth;
+  carouselContainer.style.transform = 'translateX(' + translateValue + 'px)';
+  updateDots();
+}
+
+// Función para actualizar los puntos de navegación
+function updateDots() {
+  dots.forEach(function(dot, index) {
+    dot.classList.toggle('active', index === currentIndex);
+  });
+}
+
+// Función para iniciar el temporizador
+function startTimer() {
+  timer = setInterval(nextSlide, 4000);
+}
+
+// Función para detener el temporizador
+function stopTimer() {
+  clearInterval(timer);
+}
+
+// Agrega eventos a los botones de navegación
+prevButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  prevSlide();
+  stopTimer();
+  startTimer();
+});
+
+nextButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  nextSlide();
+  stopTimer();
+  startTimer();
+});
+
+// Agrega eventos a los puntos de navegación
+dots.forEach(function(dot, index) {
+  dot.addEventListener('click', function(e) {
+    e.preventDefault();
+    currentIndex = index;
+    updateCarousel();
+    stopTimer();
+    startTimer();
+  });
+});
+
+// Inicia el carrusel
+updateCarousel();
+startTimer();
+
+
+
 
 
 
